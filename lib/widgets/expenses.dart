ffa82f87650983ea7cd6902fab4e61a39c405a00
Cypher8 <seera.kaenkaew8@gmail.com>
2023-09-27 16:19:09 +0700

    feat: Responsive & adaptive

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -29,6 +29,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+        useSafeArea: true,
         isScrollControlled: true,
         context: context,
         builder: (ctx) => NewExpense(onAddExpense: _addExpense));
@@ -62,6 +63,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -72,6 +75,7 @@ class _ExpensesState extends State<Expenses> {
         onRemoveExpense: _removeExpense,
       );
     }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter ExpenseTracker'),
@@ -82,12 +86,25 @@ class _ExpensesState extends State<Expenses> {
           )
         ],
       ),
-      body: Column(children: [
-        Chart(expenses: _registeredExpenses),
-        Expanded(
-          child: mainContent,
-        )
-      ]),
+      body: width <= 600
+          ? Column(children: [
+              Chart(expenses: _registeredExpenses),
+              Expanded(
+                child: mainContent,
+              ),
+            ])
+          : Row(
+              children: [
+                Expanded(
+                  child: Chart(
+                    expenses: _registeredExpenses,
+                  ),
+                ),
+                Expanded(
+                  child: mainContent,
+                )
+              ],
+            ),
     );
   }
 }
